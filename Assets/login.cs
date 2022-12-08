e6fde67300f6907ceb63428df9734bdd1620937d
daniel <danield880527@gmial.com>
2022-12-08 18:15:09 +0800

    fakerloginui

---
+++ Assets/login.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class login : MonoBehaviour
+{
+    public Button usrelogin;
+    public TMP_InputField UserName;
+    public TMP_InputField PassWord;
+    public TMP_Text Mistake;
+    public TMP_Text Register;
+
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnButton()
+ {
+    string UserNam = UserName.text;
+    string PassWor = PassWord.text;
+    if(UserNam == "ntnu" && PassWor == "123")
+    {
+        Register.gameObject.SetActive(true);
+        StartCoroutine(Disappear());
+      //  ScenManager.LoadScene(1);
+    }
+    else
+    {
+        Mistake.gameObject.SetActive(true);
+        StartCoroutine(Disappear());
+    }
+ }
+ IEnumerator Disappear()
+ {
+    yield return new WaitForSeconds(2);
+    Mistake.gameObject.SetActive(false);
+    Register.gameObject.SetActive(false);
+ }
+ }
+
+
---
+++ Assets/windowscript2.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class windowscript2 : MonoBehaviour
+{
+    [SerializeField] Button CloseBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CloseBtn.onClick.AddListener(() =>
+        {
+            print(GameManager.score);
+            gameObject.SetActive(false); //�|�۰ʧP�_gameObject�N�O�ثe�{��(component)�Ҧb��GameObject�����A�������wCloseBtn�A�_�h�N�|��CloseBtn�ۤv����
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
